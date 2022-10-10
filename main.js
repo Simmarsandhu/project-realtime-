2ec7f16f021d756c25726d574114f53b084c51ae
Simmarsandhu <77791732+Simmarsandhu@users.noreply.github.com>
2022-10-10 18:49:09 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+noseX=0;
+noseY=0
+wristleftY=0;
+wristrightY=0
+difference=0;
+
+function preload(){
+
+}
+function setup(){
+ video= createCapture(VIDEO);
+ video.size(550,500);
+ canvas = createCanvas(550,500);
+ canvas.position(900,150);
+
+ pose_net= ml5.poseNet(video,modelLoaded);
+ pose_net.on("pose",gotposes);
+ 
+}
+function modelLoaded(){
+    console.log("model loaded");
+}
+function gotposes(results){
+    if(results.length > 0){
+        console.log(results);
+        noseX=results[0].pose.nose.x;
+        noseY=results[0].pose.nose.y;
+        wristleftY=results[0].pose.leftWrist.y;
+        wristrightY=results[0].pose.rightWrist.y;
+        difference= wristleftY-wristrightY;
+
+    }
+
+}
+function draw(){
+ background("#A77979");
+ textSize(difference);
+ fill("black");
+ text("hi ,how are you?",noseX,noseY);
+ document.getElementById("span2").innerHTML="size of the text is "+ difference + "px";
+
+}
